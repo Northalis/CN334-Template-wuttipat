fb7cd3cc828c639d3c78cd5285a95dfd67473961
Northalis <peatwtp@gmail.com>
2025-05-15 11:54:05 +0700

    push to dev after check the config and add render.yaml

--- app_frontend/next.config.mjs
+++ app_frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  images: {
+    domains: ["localhost", "user-service.onrender.com"],
+  },
 };
 
 export default nextConfig;
